a2e95be96bf7d295ce1e5db3857af262ca5dce25
Ryan Lima Ferreira <ry90ryan@gmail.com>
2025-10-02 11:19:23 -0300

    Add files via upload

---
+++ Biblioteca.java
@@ -0,0 +1,13 @@
+package exercicio;
+
+public class Biblioteca {
+
+	public void adicionarLivro(Livroo l) {
+}	
+	public void emprestarLivro(Usuario u, String codigo) {
+		
+	}
+	public void devolverLivro(Usuario u, String codigo) {
+	
+}}
+
